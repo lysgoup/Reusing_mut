7b3d962408e614532add93e48e792af49c477ca2
YunseoLee <yunseolee.dev@gmail.com>
2026-03-12 19:16:57 +0900

    [ADD] count hang, crash, normal in dry-run & make dryrun_finish file in queue

--- fuzzer/src/fuzz_main.rs
+++ fuzzer/src/fuzz_main.rs
@@ -79,8 +79,16 @@ pub fn fuzz_main(
         panic!();
     }
 
-    info!("Dry-run completed. Exiting for testing.");
-    return;
+    info!("Dry-run completed. Total inputs executed: {}", executor.local_stats.num_exec);
+
+    // Create dryrun_finish marker file in inputs directory
+    let dryrun_finish_path = depot.dirs.inputs_dir.join("dryrun_finish");
+    match fs::File::create(&dryrun_finish_path) {
+        Ok(_) => info!("Created dryrun_finish marker at {:?}", dryrun_finish_path),
+        Err(e) => warn!("Could not create dryrun_finish marker: {:?}", e),
+    }
+
+    // return;
 
     let (handles, child_count) = init_cpus_and_run_fuzzing_threads(
         bind,
